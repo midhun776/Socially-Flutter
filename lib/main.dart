e075ce8cf9d868fad7a64c0a456bf8bf345a145e
Midhun Murali <midhunmurali776@gmail.com>
2024-06-24 17:00:27 +0530

    KAN 3 Bottom Navigation Updates

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const Chatinboxscreen(),
+      home: MyHomePage(),
     );
   }
 }
--- lib/pages/DashboardScreen.dart
+++ lib/pages/DashboardScreen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:socially/pages/ChatInboxScreen.dart';
 import 'package:socially/pages/HomeScreen.dart';
 import 'package:socially/pages/ProfileScreen.dart';
 
@@ -16,7 +19,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
   final _pages = [
     const HomeScreen(),
-    const ProfileScreen(),
+    const Chatinboxscreen(),
     const HomeScreen(),
     const ProfileScreen(),
   ];
@@ -25,22 +28,105 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedIndex],
-      bottomNavigationBar: GNav(
-        backgroundColor: Color.fromARGB(255, 226, 231, 222),
-          color: Color.fromARGB(255, 160, 169, 140),
-          activeColor: Color.fromARGB(255, 83, 115, 15),
-          tabBackgroundColor: Color.fromARGB(252, 251, 255, 233),
-          gap: 5,
-          onTabChange: (index) {
-            _selectedIndex = index;
-            setState(() {});
-          },
-          tabs: const [
-              GButton(icon: Icons.home_filled, text: "Home"),
-              GButton(icon: Icons.map, text: "Find"),
-              GButton(icon: Icons.group, text: "Community"),
-              GButton(icon: Icons.person, text: "Profile"),
-      ]),
+      bottomNavigationBar:
+          BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            notchMargin: 5,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                GestureDetector(
+                  onTap: () { setState(() {
+                    _selectedIndex = 0;
+                  }); },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.home),
+                        Text("Home",
+                        style: TextStyle(fontSize: 10),
+                        overflow: TextOverflow.ellipsis,)
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () { setState(() {
+                    _selectedIndex = 1;
+                  }); },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.map),
+                        Text("Connect",
+                          style: TextStyle(fontSize: 10),
+                          overflow: TextOverflow.ellipsis,)
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () { setState(() {
+                    _selectedIndex = 2;
+                  }); },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.notification_add),
+                        Text("Notification",
+                          style: TextStyle(fontSize: 10),
+                          overflow: TextOverflow.ellipsis,)
+                      ],
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () { setState(() {
+                    _selectedIndex = 3;
+                  }); },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        Icon(Icons.person),
+                        Text("Profile",
+                          style: TextStyle(fontSize: 10),
+                          overflow: TextOverflow.ellipsis,)
+                      ],
+                    ),
+                  ),
+                ),
+              ]
+            ),
+          ),
+      // GNav(
+      //   backgroundColor: Color.fromARGB(255, 226, 231, 222),
+      //     color: Color.fromARGB(255, 160, 169, 140),
+      //     activeColor: Color.fromARGB(255, 83, 115, 15),
+      //     tabBackgroundColor: Color.fromARGB(252, 251, 255, 233),
+      //     gap: 5,
+      //     onTabChange: (index) {
+      //       _selectedIndex = index;
+      //       setState(() {});
+      //     },
+      //     tabs: const [
+      //         GButton(icon: Icons.home_filled, text: "Home"),
+      //         GButton(icon: Icons.map, text: "Find"),
+      //         GButton(icon: Icons.group, text: "Community"),
+      //         GButton(icon: Icons.person, text: "Profile"),
+      // ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton:
+      FloatingActionButton(
+        onPressed: () {},
+            child: Icon(Icons.add),
+        elevation: 2.0,  // Adjust elevation as needed
+        shape: CircleBorder(),
+      )
     );
   }
 }

d6bdbf154b243daaa8dfc49a11fdb59fab759167
Midhun Murali <midhunmurali776@gmail.com>
2024-06-27 12:48:47 +0530

    KAN 6 Empty Notification Ui Added

--- lib/pages/NotificationScreen.dart
+++ lib/pages/NotificationScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:socially/Resources/colorresources.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
@@ -11,7 +12,24 @@ class _NotificationScreenState extends State<NotificationScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text("Notifications"),
+        leading: Icon(Icons.arrow_back),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(60.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Image.asset("assets/images/empty_notifications.png"),
+            ),
+            Text("Empty Notifications", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: ColorResources.SecondaryColor),),
+            Text("We'll notify you when there is something new!", style:  TextStyle(fontSize: 15), textAlign: TextAlign.center,)
+          ],
+        ),
+      ),
     );
   }
 }
